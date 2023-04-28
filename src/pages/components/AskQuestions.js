c9919c994b12737dd9dc06b95ab8cdc5e116065a
Romio <bsandala23@gmail.com>
2023-04-28 05:41:13 +0200

    Final commit on the ask question page

--- src/pages/components/AskQuestions.js
+++ src/pages/components/AskQuestions.js
@@ -78,15 +78,112 @@ function AskQuestion() {
             </div>
           </div>
           <div className={styles.contentBox2}>
-            <h2>Recent Topics</h2>
-            <li>How long is free trail</li>
-            <label className={styles.textC}>3 days, 5 hours ago</label>
-            <li>How long is free trail</li>
-            <label className={styles.textC}>3 days, 5 hours ago</label>
-            <li>How long is free trail</li>
-            <label className={styles.textC}>3 days, 5 hours ago</label>
+          <h2 className={styles.heading3}>Recent Topics</h2>
+              <li>How long is free trail</li>
+              <label className={styles.textC}>3 days, 5 hours ago</label>
+              <li>How long is free trail</li>
+              <label className={styles.textC}>3 days, 5 hours ago</label>
+              <li>How long is free trail</li>
+              <label className={styles.textC}>3 days, 5 hours ago</label>
           </div>
         </div>
+        <div className={styles.contentBox}>
+          <div className={styles.contentBox1}>
+            <h2 className={styles.heading2}>Community</h2>
+            <div className={styles.subBox1}>
+                <div className={styles.subBox1A}>
+                  <ChatIcon marginEnd={2} width={6} />
+                  <label>How long is the free trial ?</label>
+                  <br />
+                  <label className={styles.text1B}>How long is the free rial period ?</label>
+                </div>
+                <div className={styles.topicCount}>
+                  <label className={styles.textA}>1</label>
+                  <br />
+                  <label className={styles.textB}>Topic</label>
+                </div>
+                <div className={styles.vertical}></div>
+                <div className={styles.RepliesCount}>
+                  <label className={styles.textA}>0</label>
+                  <br />
+                  <label className={styles.textB}>Replies</label>
+                </div>
+                <div className={styles.subBox1B}>
+                  <img src='/images/john doe.jpg' className={styles.img1} />
+                  <div className={styles.nameBox2}>
+                      <label className={styles.nameText2} >Sandala Barby</label>
+                      <br />
+                      <label className={styles.textB}>3 Days, 5 hours ago</label>
+                  </div>
+                </div>
+            </div>
+          </div>
+          <div className={styles.contentBox2}>
+            <h2 className={styles.heading3}>Stats</h2>
+            <label className={styles.label3}>Registerd Users</label><label>23</label>
+            <br />
+            <label className={styles.label3}>Topics</label><span className={styles.tab}></span><label>13</label>
+            <br />
+            <label className={styles.label3}>Replies</label> <span className={styles.tab}></span><label>2</label>
+          </div>
+        </div>
+        <div className={styles.contentBox1}>
+            <h2 className={styles.heading2}>Informative</h2>
+            <div className={styles.subBox1}>
+                <div className={styles.subBox1A}>
+                  <ChatIcon marginEnd={2} width={6} />
+                  <label>How long is the free trial ?</label>
+                  <br />
+                  <label className={styles.text1B}>How long is the free rial period ?</label>
+                </div>
+                <div className={styles.topicCount}>
+                  <label className={styles.textA}>1</label>
+                  <br />
+                  <label className={styles.textB}>Topic</label>
+                </div>
+                <div className={styles.vertical}></div>
+                <div className={styles.RepliesCount}>
+                  <label className={styles.textA}>0</label>
+                  <br />
+                  <label className={styles.textB}>Replies</label>
+                </div>
+                <div className={styles.subBox1B}>
+                  <img src='/images/john doe.jpg' className={styles.img1} />
+                  <div className={styles.nameBox2}>
+                      <label className={styles.nameText2} >Sandala Barby</label>
+                      <br />
+                      <label className={styles.textB}>3 Days, 5 hours ago</label>
+                  </div>
+                </div>
+            </div>
+            <div className={styles.subBox1}>
+                <div className={styles.subBox1A}>
+                  <ChatIcon marginEnd={2} width={6} />
+                  <label>How long is the free trial ?</label>
+                  <br />
+                  <label className={styles.text1B}>How long is the free rial period ?</label>
+                </div>
+                <div className={styles.topicCount}>
+                  <label className={styles.textA}>1</label>
+                  <br />
+                  <label className={styles.textB}>Topic</label>
+                </div>
+                <div className={styles.vertical}></div>
+                <div className={styles.RepliesCount}>
+                  <label className={styles.textA}>0</label>
+                  <br />
+                  <label className={styles.textB}>Replies</label>
+                </div>
+                <div className={styles.subBox1B}>
+                  <img src='/images/john doe.jpg' className={styles.img1} />
+                  <div className={styles.nameBox2}>
+                      <label className={styles.nameText2} >Sandala Barby</label>
+                      <br />
+                      <label className={styles.textB}>3 Days, 5 hours ago</label>
+                  </div>
+                </div>
+            </div>
+          </div>
     </div>
   )
 }
