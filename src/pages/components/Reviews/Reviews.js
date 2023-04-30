36776b21eecc40be46258ab5a655c7885f891f86
Romio <bsandala23@gmail.com>
2023-04-30 08:02:22 +0200

    Final changes on the Review page

--- src/pages/components/Reviews/Reviews.js
+++ src/pages/components/Reviews/Reviews.js
@@ -3,16 +3,14 @@ import React from 'react'
 import { Button, Center, Container, Flex, Image, Select, Box, Text } from '@chakra-ui/react';
 
 import styles from '../../../styles/Reviews.module.css'
-import { Carousel, CarouselItem } from 'react-bootstrap';
-import SlideData from './ReviwSlideData';
 import { ChevronLeftIcon, ChevronRightIcon, StarIcon } from '@chakra-ui/icons';
-// import { Carousel } from "react-responsive-carousel";
-// import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function Reviews() {
   return (
     <Container maxWidth={1000} className={styles.mainDivR}>
-        <Text p={4} fontWeight={500}>Select Brand</Text>
+       
+       {/*Brand Box */}
+        <Text marginBottom={2} fontSize={20} fontWeight={700}>Select Brand</Text>
          <Flex miniWidth={'max-content'} gap={2}>
             <Select  placeholder='-Select Brand-'>
                 <option>Mercedes Benz Truck</option>
@@ -22,83 +20,138 @@ function Reviews() {
             </Select>
             <Button bg={'blue.600'} color={'#fff'} >Drivers</Button> 
          </Flex>
-        <Center>
-            <div className={styles.carousel}>
-
-                    <ChevronLeftIcon marginTop={20}  width={8} height={8}/>
+        <Center marginTop={10}>
+            <Box 
+                width={1000}
+                bg={'linear-gradient(180deg, #6385FF -3.21%, #F2A8A8 71.13%)'}
+                >
+                <Flex>
+                    <ChevronLeftIcon  width={20} height={20} className={styles.leftIcon}/>                   
                     <Image
-                        margin={1}
-                        borderRadius={20} 
-                        width={500} 
-                        height={250} 
-                        src={'/images/truck2.jpg'} />
-                    <ChevronRightIcon  width={8} height={8} marginTop={20}/>
-            </div>
+                        margin={10}
+                        borderRadius={100} 
+                        width={800} 
+                        height={400} 
+                        src={'/images/truck5.jpg'} />
+                    <ChevronRightIcon
+                      width={20} 
+                      height={20}
+                      className={styles.RightIcon}/>
+                </Flex>
+            </Box>
         </Center>
-        <Center marginTop={-20} marginBottom={20}>
-            <Button bg={'blue.600'} color={'white'} width={100} >Rate</Button>
+        <Center marginTop={-15} marginBottom={20}>
+            <Button 
+                bg={'blue.600'} 
+                color={'white'} 
+                width={100} 
+                fontFamily={'Playfair Display'}
+                fontSize={20}>Rate</Button>
         </Center>
-        <Text fontWeight={500}>Similar Trucks</Text>
+
+        {/* Simila Trucks */}
+
+        <Text fontWeight={700} fontSize={20}>Similar Trucks</Text>
         <Flex gap={20} paddingTop={4}>
             <Box>
-                <Image borderRadius={9} width={270} height={200} src='/images/truck3.jpg' />
+                <Image borderRadius={9} width={270} height={200} src='/images/truck1.jpg' />
                 <Center>    
-                    <Button bg={'blue.600'} marginTop={-20} color={'white'}>Mercedes-Benz Aroes</Button>
+                    <Button  fontFamily={'Playfair Display'} bg={'blue.800'} marginTop={-20} color={'white'}>Mercedes-Benz Aroes</Button>
                 </Center>
             </Box>
             <Box>
-                <Image borderRadius={9} width={270} height={200} src='/images/truck3.jpg' />
+                <Image borderRadius={9} width={270} height={200} src='/images/truck2.jpg' />
                 <Center>    
-                    <Button bg={'blue.600'} marginTop={-20} color={'white'}>Mercedes-Benz Aroes</Button>
+                    <Button  fontFamily={'Playfair Display'} bg={'blue.800'} marginTop={-20} color={'white'}>Mercedes-Benz Aroes</Button>
                 </Center>
             </Box>
             <Box>
-                <Image borderRadius={9} width={270} height={200} src='/images/truck3.jpg' />
+                <Image borderRadius={9} width={270} height={200}  src='/images/truck5.jpg' />
                 <Center>    
-                    <Button bg={'blue.600'} marginTop={-20} color={'white'}>Mercedes-Benz Aroes</Button>
+                    <Button  fontFamily={'Playfair Display'} bg={'blue.800'} marginTop={-20} color={'white'}>Mercedes-Benz Aroes</Button>
                 </Center>
             </Box>
         </Flex>
 
         {/*Pinned Reviews */}
-        <Text paddingBottom={5}>Pinned Reviews</Text>
-        <Flex gap={20}>
-            <div>
-                <Flex>
-                    <Image width={100} height={20} borderRadius={50} src='/images/truck3.jpg' className={styles.img3} />
-                    <StarIcon />
-                    <StarIcon />
-                    <StarIcon />
-                    <StarIcon />
-                    <StarIcon />
-                </Flex>
-                <Text fontWeight={600}>Mrs. Hart</Text>
-                <Text>My Husband loves this truck</Text>
-                <label>On <Link href={''}>Mercedes-Benz Zetros</Link></label>
-            </div>
-            <div>
-                <Image width={800}  height={200} borderRadius={8} src='/images/truck1.jpg' className={styles.img3} />
-                <StarIcon />
-                <StarIcon />
-                <StarIcon />
-                <StarIcon />
-                <StarIcon />
-                <Text fontWeight={600}>Mrs. Hart</Text>
-                <Text>My Husband loves this truck</Text>
-                <label>On <Link href={''}>Mercedes-Benz Zetros</Link></label>
-            </div>
-            <div>
-                <Image width={800} height={200} borderRadius={8} src='/images/truck7.jpg' className={styles.img3} />
-                <StarIcon />
-                <StarIcon />
-                <StarIcon />
-                <StarIcon />
-                <StarIcon />
-                <Text fontWeight={600}>Mrs. Hart</Text>
-                <Text>My Husband loves this truck</Text>
-                <label>On <Link href={''}>Mercedes-Benz Zetros</Link></label>
-            </div>
-        </Flex>
+  
+        <Text marginTop={20} fontSize={20} fontWeight={700}>Pinned Reviews</Text>
+        <Center>
+            <Flex gap={20} marginTop={20}>
+                <Box 
+                    width={250}
+                    bg={'#E6F3FF'} 
+                    padding={5} borderRadius={8}  
+                    border={ '1px solid #EFEFF0'}
+                    boxShadow={" 0px 2px 4px rgba(0, 0, 0, 0.15)"}>
+                    <Flex>
+                        <Image width={110} height={110} borderRadius={55} marginTop={-20} src='/images/john Doe.jpg' className={styles.img3} />
+                        <StarIcon  color={'yellow.400'}/>
+                        <StarIcon color={'yellow.400'} />
+                        <StarIcon color={'yellow.400'}/>
+                        <StarIcon color={'yellow.400'}/>
+                        <StarIcon color={'yellow.400'}/>
+                    </Flex>
+                    <Center>
+                        <Text fontWeight={700}  fontFamily={'Playfair Display'} marginTop={2}>Mrs. Hart</Text>
+                    </Center>
+                    <Center padding={3}>
+                        <Text textAlign={'center'} fontWeight={500}>My Husband loves this truck</Text>
+                    </Center>
+                    <Center marginTop={20}>
+                        <Text fontWeight={600}>On <Link  href={''}>Mercedes-Benz Zetros</Link></Text>
+                    </Center>
+                </Box>
+                <Box 
+                    width={250}
+                    bg={'#E6F3FF'} 
+                    padding={5} borderRadius={8}  
+                    border={ '1px solid #EFEFF0'}
+                    boxShadow={" 0px 2px 4px rgba(0, 0, 0, 0.15)"}>
+                    <Flex>
+                        <Image width={110} height={110} borderRadius={55} marginTop={-20} src='/images/john Doe.jpg' className={styles.img3} />
+                        <StarIcon  color={'yellow.400'}/>
+                        <StarIcon color={'yellow.400'} />
+                        <StarIcon color={'yellow.400'}/>
+                        <StarIcon color={'yellow.400'}/>
+                        <StarIcon color={'yellow.400'}/>
+                    </Flex>
+                    <Center>
+                        <Text fontWeight={700}  fontFamily={'Playfair Display'} marginTop={2}>Mrs. Hart</Text>
+                    </Center>
+                    <Center padding={3}>
+                        <Text textAlign={'center'} fontWeight={500}>My Husband loves this truck</Text>
+                    </Center>
+                    <Center marginTop={20}>
+                        <Text fontWeight={600}>On <Link color={''}  href={''}>Mercedes-Benz Zetros</Link></Text>
+                    </Center>
+                </Box>
+                <Box 
+                    width={250}
+                    bg={'#E6F3FF'} 
+                    padding={5} borderRadius={8}  
+                    border={ '1px solid #EFEFF0'}
+                    boxShadow={" 0px 2px 4px rgba(0, 0, 0, 0.15)"}>
+                    <Flex>
+                        <Image width={110} height={110} borderRadius={55} marginTop={-20} src='/images/john Doe.jpg' className={styles.img3} />
+                        <StarIcon  color={'yellow.400'}/>
+                        <StarIcon color={'yellow.400'} />
+                        <StarIcon color={'yellow.400'}/>
+                        <StarIcon color={'yellow.400'}/>
+                        <StarIcon color={'yellow.400'}/>
+                    </Flex>
+                    <Center>
+                        <Text fontWeight={700}  fontFamily={'Playfair Display'} marginTop={2}>Mrs. Hart</Text>
+                    </Center>
+                    <Center padding={3}>
+                        <Text textAlign={'center'} fontWeight={500}>My Husband loves this truck</Text>
+                    </Center>
+                    <Center marginTop={20}>
+                        <Text fontWeight={600}>On <Link color={''}  href={''}>Mercedes-Benz Zetros</Link></Text>
+                    </Center>
+                </Box>
+            </Flex>
+        </Center>
     </Container>
   )
 }
